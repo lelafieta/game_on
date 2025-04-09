f4457bddb72e1daf212279717d1f535b2731c745
Lela Fieta <lelafieta11@gmail.com>
2025-04-09 23:56:06 +0100

    Creating Organizers Page || Create organizers grid

--- lib/src/features/organizers/presentation/pages/organizer_page.dart
+++ lib/src/features/organizers/presentation/pages/organizer_page.dart
@@ -44,74 +44,128 @@ class _OrganizerPageState extends State<OrganizerPage> {
             padding: const EdgeInsets.all(16.0),
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
-              crossAxisSpacing: 5.0,
-              mainAxisSpacing: 10.0,
+              crossAxisSpacing: 16.0,
+              mainAxisSpacing: 16.0,
               childAspectRatio: 0.8,
             ),
             itemCount: 10, // Number of elements (2 to 10)
             itemBuilder: (context, index) {
-              return Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                elevation: 2,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: ClipRRect(
-                        borderRadius: const BorderRadius.vertical(
-                          top: Radius.circular(12.0),
+              return Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.09),
+                          blurRadius: 12,
+                          offset: const Offset(0, 4),
                         ),
-                        child: Image.network(
-                          'https://scontent.flad5-1.fna.fbcdn.net/v/t39.30808-6/393362262_295776626562301_407325209243642260_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=Ic8klsng5fwQ7kNvwHzz7Nf&_nc_oc=AdnfpQl6hbBQJYG8NDxfcltgT7wZ2nUGUigL3r7qNjmpj0Zf6MEkoTUMmUzVAJuxd5kEjIE0QOLsAaVv8y2WJx_I&_nc_zt=23&_nc_ht=scontent.flad5-1.fna&_nc_gid=zJ3PK8hA85pmF3pUjOPzWw&oh=00_AfHAR3go9D1FnCrF0S0MbAOvYO4fz_Utmyp8lJAfmIuLAg&oe=67FCAEB5',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
+                      ],
                     ),
-                    Expanded(
-                      flex: 0,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Organizer ${index + 1}',
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 14.0,
-                                color: AppColors.primary,
-                                overflow: TextOverflow.ellipsis,
-                              ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: ClipRRect(
+                            borderRadius: const BorderRadius.vertical(
+                              top: Radius.circular(12.0),
                             ),
-                            const SizedBox(height: 4.0),
-                            Row(
+                            child: Image.network(
+                              'https://scontent.flad5-1.fna.fbcdn.net/v/t39.30808-6/393362262_295776626562301_407325209243642260_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=Ic8klsng5fwQ7kNvwHzz7Nf&_nc_oc=AdnfpQl6hbBQJYG8NDxfcltgT7wZ2nUGUigL3r7qNjmpj0Zf6MEkoTUMmUzVAJuxd5kEjIE0QOLsAaVv8y2WJx_I&_nc_zt=23&_nc_ht=scontent.flad5-1.fna&_nc_gid=zJ3PK8hA85pmF3pUjOPzWw&oh=00_AfHAR3go9D1FnCrF0S0MbAOvYO4fz_Utmyp8lJAfmIuLAg&oe=67FCAEB5',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 0,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                SvgPicture.asset(
-                                  AppIcons.marker,
-                                  width: 14,
+                                Text(
+                                  'Organizer ${index + 1}',
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14.0,
+                                    color: AppColors.primary,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
                                 ),
-                                const SizedBox(width: 4.0),
-                                const Text(
-                                  'Luanda, Benfica',
-                                  style: TextStyle(fontSize: 12.0),
+                                const SizedBox(height: 4.0),
+                                Row(
+                                  children: [
+                                    SvgPicture.asset(
+                                      AppIcons.marker,
+                                      width: 14,
+                                    ),
+                                    const SizedBox(width: 4.0),
+                                    const Text(
+                                      'Luanda, Benfica',
+                                      style: TextStyle(fontSize: 12.0),
+                                    ),
+                                  ],
                                 ),
+                                const SizedBox(height: 4.0),
+                                Text(
+                                  'Desde: 2023-01-01',
+                                  style: const TextStyle(fontSize: 12.0),
+                                ),
+                                const SizedBox(height: 4.0),
                               ],
                             ),
-                            const SizedBox(height: 4.0),
-                            Text(
-                              'Desde: 2023-01-01',
-                              style: const TextStyle(fontSize: 12.0),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    right: 0,
+                    top: 15,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: AppColors.primary,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10.0),
+                          bottomLeft: Radius.circular(10.0),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.09),
+                            blurRadius: 12,
+                            offset: const Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 8.0,
+                        vertical: 4.0,
+                      ),
+                      child: RichText(
+                        text: TextSpan(
+                          text: '0',
+                          style: const TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: ' TORNEIOS',
+                              style: TextStyle(
+                                fontSize: 14.0,
+                                color: Colors.white.withOpacity(0.7),
+                              ),
                             ),
-                            const SizedBox(height: 4.0),
                           ],
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  )
+                ],
               );
             },
           ),
