2617272bc17fc1f79de2b9d8346f400b83e7b742
Lela Fieta <lelafieta11@gmail.com>
2025-04-16 00:29:08 +0100

    Competition List Item

--- lib/src/features/competitions/presentation/pages/my_competitions_list_page.dart
+++ lib/src/features/competitions/presentation/pages/my_competitions_list_page.dart
@@ -1,7 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../../config/themes/app_colors.dart';
+import '../../../../core/resources/app_icons.dart';
 import '../../../../core/strings/app_strings.dart';
 
 class MyCompetitionsListPage extends StatelessWidget {
@@ -86,11 +88,37 @@ class MyCompetitionsListPage extends StatelessWidget {
                       isScrollable: true,
                       padding: EdgeInsets.zero,
                       tabAlignment: TabAlignment.center,
-                      tabs: const [
-                        Tab(text: "Todos"),
-                        Tab(text: "Pendentes"),
+                      tabs: [
+                        const Tab(text: "Todos"),
+                        Tab(
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 25,
+                                height: 25,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Colors.red,
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    "2",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 6,
+                              ),
+                              Text("Notificações"),
+                            ],
+                          ),
+                        ),
                         Tab(text: "Rascunho"),
-                        Tab(text: "Terminados"),
+                        Tab(text: "Mídia"),
                       ],
                     ),
                   ),
@@ -156,64 +184,219 @@ class MyCompetitionsListPage extends StatelessWidget {
                   padding: const EdgeInsets.only(top: 5),
                   child: Row(
                     children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(50),
-                        child: Container(
-                          width: 20,
-                          height: 20,
-                          child: CachedNetworkImage(
-                              imageUrl:
-                                  "https://fpfimagehandler.fpf.pt/FPFImageHandler.ashx?type=Person&id=3883014&op=t&w=325&h=378"),
+                      SvgPicture.asset(
+                        AppIcons.marker,
+                        width: 16,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text("Angola, Luanda, Luanda"),
+                    ],
+                  ),
+                ),
+                // trailing: RichText(
+                //   text: TextSpan(
+                //     children: [
+                //       TextSpan(
+                //         text: "20 ",
+                //         style: TextStyle(
+                //             fontWeight: FontWeight.bold,
+                //             color: Colors.black,
+                //             fontSize: 18,
+                //             fontFamily: AppStrings.fontFamily),
+                //       ),
+                //       TextSpan(
+                //         text: "",
+                //         style: TextStyle(
+                //             color: Colors.grey,
+                //             fontFamily: AppStrings.fontFamily),
+                //       ),
+                //     ],
+                //   ),
+                // ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: SvgPicture.asset(
+                      AppIcons.link,
+                      width: 24,
+                      height: 24,
+                    ),
+                    onPressed: () {
+                      // Add share functionality here
+                    },
+                  ),
+                  IconButton(
+                    icon: SvgPicture.asset(
+                      AppIcons.facebook,
+                      width: 24,
+                      height: 24,
+                    ),
+                    onPressed: () {
+                      // Add Facebook share functionality here
+                    },
+                  ),
+                  IconButton(
+                    icon: SvgPicture.asset(
+                      AppIcons.whatsapp,
+                      width: 24,
+                      height: 24,
+                    ),
+                    onPressed: () {
+                      // Add WhatsApp share functionality here
+                    },
+                  ),
+                  IconButton(
+                    icon: SvgPicture.asset(
+                      AppIcons.instagram,
+                      width: 24,
+                      height: 24,
+                    ),
+                    onPressed: () {
+                      // Add copy link functionality here
+                    },
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: AppColors.primary.withOpacity(0.1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            '12',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: AppColors.primary,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            'Equipes',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black54,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: AppColors.primary.withOpacity(0.1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            '8',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: AppColors.primary,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            'Jogos',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black54,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: AppColors.primary.withOpacity(0.1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            '24',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: AppColors.primary,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            'Golos',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black54,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SvgPicture.asset(
+                        AppIcons.calendar,
+                        width: 16,
+                        height: 16,
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        "Início: 01/01/2023",
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black54,
                         ),
                       ),
-                      Text("Real Madrid"),
                     ],
                   ),
-                ),
-                trailing: RichText(
-                  text: TextSpan(
+                  Row(
                     children: [
-                      TextSpan(
-                        text: "20 ",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                            fontSize: 18,
-                            fontFamily: AppStrings.fontFamily),
+                      SvgPicture.asset(
+                        AppIcons.calendarBold,
+                        width: 16,
+                        height: 16,
                       ),
-                      TextSpan(
-                        text: "Gol.",
+                      const SizedBox(width: 8),
+                      Text(
+                        "Término: 31/12/2023",
                         style: TextStyle(
-                            color: Colors.grey,
-                            fontFamily: AppStrings.fontFamily),
+                          fontSize: 14,
+                          color: Colors.black54,
+                        ),
                       ),
                     ],
                   ),
-                ),
-              ),
-              Text(
-                "Competição ${index + 1}", // Replace with competition name
-                style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                      fontWeight: FontWeight.bold,
-                      color: AppColors.primary,
-                    ),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                "Localização: Cidade XYZ", // Replace with competition location
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-              const SizedBox(height: 8),
-              Text(
-                "Estado: ${index % 2 == 0 ? 'A decorrer' : 'Terminado'}", // Replace with competition status
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: index % 2 == 0 ? Colors.green : Colors.red,
-                    ),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                "Quantidade de equipes: ${5 + index}", // Replace with team count
-                style: Theme.of(context).textTheme.bodyMedium,
+                ],
               ),
             ],
           ),
